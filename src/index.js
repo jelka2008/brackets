c30fd581709b1d6199af92e478dbc9633e4c1ee8
jelka2008 <jelka2008.rambler.ru>
2020-10-15 00:54:46 +0300

    feat: implement task resolve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let tempArrEnterBracketsStr = [];
+    let configObject = {};
+    let configArrayEqualBrackers = [];
+    let enterBracketsConfig = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        configObject = {
+            ...configObject,
+            [bracketsConfig[i][0]]: bracketsConfig[i][1],
+        };
+        enterBracketsConfig.push(bracketsConfig[i][0]);
+        if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+            configArrayEqualBrackers.push(bracketsConfig[i][1]);
+        }
+    }
+    for (let i = 0; i < str.length; i++) {
+        const lastEnterBacket =
+            tempArrEnterBracketsStr[tempArrEnterBracketsStr.length - 1];
+        if (
+            enterBracketsConfig.includes(str[i]) &&
+            !(
+                configArrayEqualBrackers.includes(lastEnterBacket) &&
+                str[i] === lastEnterBacket
+            )
+        ) {
+            tempArrEnterBracketsStr.push(str[i]);
+        } else {
+            if (
+                (tempArrEnterBracketsStr.length !== 0 &&
+                    str[i] === configObject[lastEnterBacket]) ||
+                (lastEnterBacket === "|" && str[i] === "|")
+            ) {
+                tempArrEnterBracketsStr.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return tempArrEnterBracketsStr.length === 0;
+};
